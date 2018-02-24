72b51ab08a1fb03f6a25b77400f65aa8642cc3ff
zrunker <zrunker@yahoo.com>
2018-02-24 13:24:30 +0800

    init Commit

--- app/src/main/java/cc/ibooker/zpopupwindow/MainActivity.java
+++ app/src/main/java/cc/ibooker/zpopupwindow/MainActivity.java
@@ -12,17 +12,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+        // 从屏幕顶部弹出
+        findViewById(R.id.tv1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (diyPopupWindow == null) {
                     diyPopupWindow = new DiyPopupWindow(MainActivity.this);
-                    diyPopupWindow.setAnimationStyle(cc.ibooker.zpopupwindowlib.R.style.BottomPushPopupWindow);
-
-//                diyPopupWindow.setAnimationStyle(cc.ibooker.zpopupwindowlib.R.style.TopPushPopupWindow);
-//                diyPopupWindow.showTop();
                 }
+                diyPopupWindow.setAnimationStyle(cc.ibooker.zpopupwindowlib.R.style.TopPushPopupWindow);
+                diyPopupWindow.showTop();
+            }
+        });
+
+        // 屏幕底部弹出
+        findViewById(R.id.tv2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (diyPopupWindow == null) {
+                    diyPopupWindow = new DiyPopupWindow(MainActivity.this);
+                }
+                diyPopupWindow.setAnimationStyle(cc.ibooker.zpopupwindowlib.R.style.BottomPushPopupWindow);
                 diyPopupWindow.showBottom();
+            }
+        });
+
+        // 当前控件正上方弹出
+        findViewById(R.id.tv3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (diyPopupWindow == null) {
+                    diyPopupWindow = new DiyPopupWindow(MainActivity.this);
+                }
+                diyPopupWindow.setAnimationStyle(android.R.style.Animation_Toast);
+                diyPopupWindow.showViewTop(findViewById(R.id.tv3), 0);
+            }
+        });
+
+        // 当前控件正下方弹出
+        findViewById(R.id.tv4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (diyPopupWindow == null) {
+                    diyPopupWindow = new DiyPopupWindow(MainActivity.this);
+                }
+                diyPopupWindow.setAnimationStyle(android.R.style.Animation_Toast);
+                diyPopupWindow.showViewBottom(findViewById(R.id.tv4), 0);
             }
         });
     }
